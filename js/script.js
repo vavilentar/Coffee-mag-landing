f04079c8a3fe7aa859f43fa5d8eedb99eea7b213
Roman Pchelkin <epos.91@mail.ru>
2022-04-18 10:50:22 +0300

    Fixes

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+// const tl = gsap.timeline({defaults: { duration: 1, ease: "power3.easeOut" }
+// });
+
+// tl.fromTo('.overview, .header', {opacity: 0}, {opacity: 1})
+// tl.fromTo('.overview-title', {opacity: 0}, {opacity: 1},"<75%")
+// tl.fromTo('.overview-subtitle', {opacity: 0}, {opacity: 1},"<50%")
+// tl.fromTo('.button-view', {opacity: 0}, {opacity: 1}, "<20%")
+
+// // gsap.registerPlugin(ScrollTrigger);
+
+// tl.fromTo('.info', {scrollTrigger: {
+// 	trigger: '.info',
+// 	toggleActions: "restart none none none"
+// }, opacity: 0}, {opacity: 1})
+// tl.fromTo('.featured', {scrollTrigger: '.featured', opacity: 0}, {opacity: 1})
+// tl.fromTo('.products', {scrollTrigger: '.products', opacity: 0}, {opacity: 1})
+// tl.fromTo('.promo', {scrollTrigger: '.promo', opacity: 0}, {opacity: 1})
+// tl.fromTo('.coffee-img', {scrollTrigger: '.coffee-img', opacity: 0}, {opacity: 1})
+// tl.fromTo('.stories', {scrollTrigger: '.stories', opacity: 0}, {opacity: 1})
+// tl.fromTo('.updates', {scrollTrigger: '.updates', opacity: 0}, {opacity: 1})
